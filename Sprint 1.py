54961d08d6cccbf871f570a1ff5e058454ee3963
mveronicasoares <veronicasoares4@gmail.com>
2020-02-16 17:46:57 -0300

    Parte 2 do Lab01S01: Consulta graphql para 1 repositório + requisição

--- Sprint 1.py
+++ Sprint 1.py
@@ -1,27 +1,47 @@
 import requests
+import json
 
 headers = {"Authorization": "token d9eff5073950ceffc4ff93f1a49666b780fcba73"}
 
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+def run_query(query): #Função para usar request.post
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
-        
-# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
+#Query para retorno da consulta com graphQL contendo os resultados necessários para a prática      
 query = """
 {
   search (query:"stars:>100",
-  				type: REPOSITORY, first:100) {
+  				type: REPOSITORY, first:1) {
     	nodes {
         ... on Repository {
           nameWithOwner
+          createdAt
+          pullRequests(states: MERGED){
+            totalCount
+          }
+          releases{
+            totalCount
+          }
+          updatedAt
+          primaryLanguage{
+            name
+          }
+          closedIssues : issues(states: CLOSED){
+            totalCount
+          }
+          totalIssues: issues{
+            totalCount
+          }
         }
       }
   }
-}"""
-
-print (query)
+}
+"""
+result = run_query(query)
+print (result)
+#parsed = json.loads(result)
+#print (json.dumps(parsed, ident=4))
